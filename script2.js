b4ca45c37dc8fc70dd96f579699d9808818bde5d
G-uillaume <72987948+G-uillaume@users.noreply.github.com>
2020-12-17 17:32:04 +0100

    Version presque finale (manque responsive)

--- script2.js
+++ script2.js
@@ -1,1 +1,258 @@
-setTimeout(() => window.location = 'game.html', 4000)
+/**
+ * @param {*} str Decodes a string from base64 to unicode.
+ */
+
+const b64DecodeUnicode = str => {
+    // Going backwards: from bytestream, to percent-encoding, to original string.
+    return decodeURIComponent(atob(str).split('').map(function (c) {
+        return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
+    }).join(''));
+}
+
+const shuffleArray = array => {
+    for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+    }
+    return array
+}
+
+const generateOptions = (list, ParentNode, plus, type) => {
+    for (let i = 0; i < list.length; i++) {
+        const option = document.createElement('option')
+        option.textContent = list[i]
+        if (type === 'number') {
+            option.value = i + plus
+        } else if (type === 'str') {
+            option.value = list[i]
+        }
+        if (list[i] === 'boolean') {
+            option.textContent = 'True / False'
+        }
+        ParentNode.appendChild(option)
+    }
+}
+const generateLabel = (parentNode, arr, type) => {
+    if (type === 'multiple') {
+        let grid = ['a', 'b', 'c', 'd']
+        for (let i = 1; i < 5; i++) {
+            const label = document.createElement('label')
+            label.setAttribute('for', 'answer' + i)
+            label.id = 'label' + i
+            label.setAttribute("grid-area", grid[i])
+            const radio = document.createElement('input')
+            radio.type = 'radio'
+            radio.name = 'answers'
+            radio.className = 'input'
+            radio.id = 'answer' + i
+            parentNode.appendChild(radio)
+            parentNode.appendChild(label)
+            arr.push({
+                label,
+                radio
+            })
+        }
+    } else if (type === 'boolean') {
+        let grid = ['a', 'b']
+        for (let i = 1; i < 3; i++) {
+            const label = document.createElement('label')
+            label.setAttribute('for', 'answer' + i)
+            label.id = 'label' + i
+            label.setAttribute("grid-area", grid[i])
+            label.style.fontSize = '4rem'
+            label.style.padding = '1em'
+            label.style.textAlign = 'center'
+            const radio = document.createElement('input')
+            radio.type = 'radio'
+            radio.name = 'answers'
+            radio.className = 'input'
+            radio.id = 'answer' + i
+            parentNode.appendChild(radio)
+            parentNode.appendChild(label)
+            arr.push({
+                label,
+                radio
+            })
+        }
+    }
+    return arr
+}
+const generateQuestion = (response, question, choices, questions, x) => {
+    question.textContent = b64DecodeUnicode(response.results[x].question)
+    for (let i = 0; i < choices.length; i++) {
+        questions[i].label.textContent = choices[i]
+        questions[i].radio.value = choices[i]
+    }
+    questions[0].radio.checked = true
+}
+
+/* ---- Page welcome ---- */
+const main = document.createElement('main')
+const welcomeDiv = document.createElement('div')
+welcomeDiv.id = 'welcome'
+const welcomeH1 = document.createElement('h1')
+welcomeH1.textContent = 'Welcome to the trivia game !'
+welcomeDiv.appendChild(welcomeH1)
+main.appendChild(welcomeDiv)
+document.body.appendChild(main)
+setTimeout(() => {
+    main.innerHTML = ''
+    play()
+}, 1)
+
+/* ---- Page choix options --- */
+function play() {
+    const categoryList = ['General Knowledge', 'Entertainment: Books', 'Entertainment: Film', 'Entertainment: Music', 'Entertainment: Musicals & Theatres', 'Entertainment: Television', 'Entertainment: Video Games', 'Entertainment: Board Games', 'Science & Nature', 'Science: Computers', 'Science: Mathematics', 'Mythology', 'Sports', 'Geography', 'History', 'Politics', 'Art', 'Celebrities', 'Animals', 'Vehicles', 'Entertainment: Comics', 'Science: Gadgets', 'Entertainment: Japanese Anime & Manga', 'Entertainment: Cartoon & Animations']
+    const difficultyList = ['easy', 'medium', 'hard']
+    const numList = [5, 10, 15, 20]
+    const typeList = ['multiple', 'boolean']
+    let step = 'category'
+    const choice = document.createElement('div')
+    choice.id = 'category'
+    const choiceLabel = document.createElement('label')
+    choiceLabel.setAttribute('for', 'categoryChoice')
+    choiceLabel.textContent = 'Choose a category :'
+    const choiceSelect = document.createElement('select')
+    choiceSelect.id = 'categoryChoice'
+    generateOptions(categoryList, choiceSelect, 9, 'number')
+    const button = document.createElement('button')
+    button.id = 'send'
+    button.textContent = 'Enter'
+    choice.appendChild(choiceLabel)
+    choice.appendChild(choiceSelect)
+    choice.appendChild(button)
+    main.appendChild(choice)
+    let category
+    let difficulty
+    let number
+    let type
+    let question
+    button.addEventListener('click', () => {
+        if (step === 'category') {
+            category = choiceSelect.value
+            choiceSelect.innerHTML = ''
+            choiceLabel.textContent = 'Choose a difficulty :'
+            generateOptions(difficultyList, choiceSelect, 0, 'str')
+            step = 'difficulty'
+        } else if (step === 'difficulty') {
+            difficulty = choiceSelect.value
+            choiceSelect.innerHTML = ''
+            choiceLabel.textContent = 'How many questions do you want ?'
+            generateOptions(numList, choiceSelect, 0, 'str')
+            step = 'num'
+        } else if (step === 'num') {
+            number = choiceSelect.value
+            choiceSelect.innerHTML = ''
+            choiceLabel.textContent = 'What kind of quizz do you want ?'
+            generateOptions(typeList, choiceSelect, 0, 'str')
+            button.textContent = 'GO !'
+            step = 'type'
+        } else if (step === 'type') {
+            type = choiceSelect.value
+            choice.innerHTML = ''
+            choice.id = 'quizz'
+            const quizzH2 = document.createElement('h2')
+            const divPropositions = document.createElement('div')
+            divPropositions.id = 'propositions'
+            const divBtn = document.createElement('div')
+            divBtn.id = 'buttons'
+            const valid = document.createElement('button')
+            valid.id = 'valid'
+            valid.textContent = 'Validate your answer'
+            const next = document.createElement('button')
+            next.id = 'next'
+            next.textContent = 'Next question'
+            divBtn.appendChild(valid)
+            divBtn.appendChild(next)
+            choice.appendChild(quizzH2)
+            choice.appendChild(divPropositions)
+            choice.appendChild(divBtn)
+            questions = []
+            generateLabel(divPropositions, questions, type)
+            const inputs = document.querySelectorAll('input')
+            fetch('https://opentdb.com/api.php?amount=' + number + '&category=' + category + '&difficulty=' + difficulty + '&type=' + type + '&encode=base64')
+                .then(response => response.json())
+                .then(response => {
+                    let i = 0
+                    let score = 0
+                    let choices = []
+                    choices.push(b64DecodeUnicode(response.results[i].correct_answer))
+                    for (let elem of response.results[i].incorrect_answers) {
+                        choices.push(b64DecodeUnicode(elem))
+                    }
+                    choices = shuffleArray(choices)
+                    generateQuestion(response, quizzH2, choices, questions, i)
+                    next.disabled = true
+                    valid.addEventListener('click', () => {
+
+                        // console.log(b64DecodeUnicode(response.results[i].correct_answer))
+                        for (let elem of inputs) {
+                            if (elem.checked) {
+                                if (elem.value == b64DecodeUnicode(response.results[i].correct_answer)) {
+                                    score++
+                                    const right = document.createElement('div')
+                                    right.id = 'right'
+                                    const h3 = document.createElement('h3')
+                                    h3.textContent = 'WELL DONE !!!'
+                                    const p = document.createElement('p')
+                                    p.innerHTML = 'Well done ! You\'ve got ' + score + ' points !'
+                                    right.appendChild(h3)
+                                    right.appendChild(p)
+                                    main.appendChild(right)
+                                    setTimeout(() => {
+                                        main.removeChild(right)
+                                        next.disabled = false
+                                    }, 1000)
+                                } else {
+                                    const wrong = document.createElement('div')
+                                    wrong.id = 'wrong'
+                                    const h3 = document.createElement('h3')
+                                    h3.textContent = 'WRONG !!!'
+                                    const p = document.createElement('p')
+                                    p.innerHTML = 'The correct answer was ' + b64DecodeUnicode(response.results[i].correct_answer) + ' !<br>You\'ve got ' + score + ' points !'
+                                    wrong.appendChild(h3)
+                                    wrong.appendChild(p)
+                                    main.appendChild(wrong)
+                                    setTimeout(() => {
+                                        main.removeChild(wrong)
+                                        next.disabled = false
+                                    }, 2000)
+                                }
+                            }
+                            elem.disabled = true
+                        }
+                        if (i >= number - 1) {
+                            next.textContent = 'FINISH'
+                        }
+                        valid.disabled = true
+                    })
+                    next.addEventListener('click', () => {
+                        if (i >= number - 1) {
+                            choice.innerHTML = ''
+                            choice.id = 'result'
+                            const h1 = document.createElement('h1')
+                            h1.textContent = 'The game is over! You have ' + score + ' out of ' + number + ' points!'
+                            const btnPlayAgain = document.createElement('button')
+                            btnPlayAgain.textContent = 'Want to play again ?'
+                            choice.appendChild(h1)
+                            choice.appendChild(btnPlayAgain)
+                            btnPlayAgain.addEventListener('click', play)
+                        }
+                        for (let elem of inputs) {
+                            elem.disabled = false
+                        }
+                        i++
+                        choices = []
+                        choices.push(b64DecodeUnicode(response.results[i].correct_answer))
+                        for (let elem of response.results[i].incorrect_answers) {
+                            choices.push(b64DecodeUnicode(elem))
+                        }
+                        choices = shuffleArray(choices)
+                        generateQuestion(response, quizzH2, choices, questions, i)
+                        next.disabled = true
+                        valid.disabled = false
+                    })
+                })
+        }
+    })
+}
